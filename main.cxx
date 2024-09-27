708473e66fcc496c6a2587f6116863a21bef21d3
borz7zy <borz7zy@yandex.ru>
2024-09-27 20:55:22 +0200

    Removed extra loading fields from config

--- main.cxx
+++ main.cxx
@@ -14,18 +14,14 @@ int main()
 
         std::string minorScripts = config->get("minor_scripts");
         std::string mainScript = config->get("main_script");
-        std::string plugins = config->get("plugins");
         int ticks = std::stoi(config->get("ticks"));
-
-        std::vector<std::string> fullPlugins;
-        processScripts(plugins, plPath, fullPlugins);
 
         std::vector<std::string> fullMinorScripts;
         processScripts(minorScripts, misPath, fullMinorScripts);
 
         std::string mainScriptFullPath = masPath + "/" + mainScript;
 
-        Core c(fullMinorScripts, mainScriptFullPath);
+        Core c(plPath, fullMinorScripts, mainScriptFullPath);
         Core *core = &c;
 
         TickSys tickSys(ticks, tick_update);
