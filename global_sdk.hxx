df040b47bc004be64dc2eee823a55e1ba0cfbf4e
borz7zy <borz7zy@yandex.ru>
2024-09-28 15:02:55 +0200

    added sdk

---
+++ global_sdk.hxx
@@ -0,0 +1,25 @@
+#ifndef _GLOBAL_SDK_HXX
+#define _GLOBAL_SDK_HXX
+
+#if defined(_MSC_VER)
+#define PACKED
+#else
+#define PACKED __attribute__((packed))
+#endif
+
+extern "C"
+{
+#include "lauxlib.h"
+#include "lua.h"
+#include "luaconf.h"
+#include "lualib.h"
+}
+
+typedef int (*IntFunctionPtr)(lua_State *L);
+
+typedef struct myNative_Function_List
+{
+    const char *nativeName PACKED;
+    IntFunctionPtr funcPtr PACKED;
+} PACKED Native_Function_List;
+#endif
