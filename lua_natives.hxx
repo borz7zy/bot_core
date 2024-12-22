c1cc720d36abc0d15626ebdd60503eddf569fbb1
borz7zy <borz7zy@yandex.ru>
2024-12-22 18:38:18 +0100

    integration of asynchronous http class into lua

--- lua_natives.hxx
+++ lua_natives.hxx
@@ -3,6 +3,9 @@
 
 #include <vector>
 #include "global_sdk.hxx"
+#include "asynchttp.hxx" // Include AsyncHttp
+#include <string>
+#include <memory>
 
 #define REGISTER_NATIVES_NAME "RegisterNatives"
 
@@ -18,11 +21,22 @@ private:
     static int native_GetEnv(lua_State *L); // GetEnv
     static int native_print(lua_State *L);  // print
 
-    const Native_Function_List native_list[4] = {
+    // Async http
+    static int native_http_request(lua_State *L);
+    static int native_http_is_complete(lua_State *L);
+    static int native_http_get_result(lua_State *L);
+    static int native_http_set_bind_address(lua_State *L);
+
+    const Native_Function_List native_list[8] = {
         {"load_module", safe_require},
         {"GetEnv", native_GetEnv},
         {"print", native_print},
+        {"http_request", native_http_request},
+        {"http_is_complete", native_http_is_complete},
+        {"http_get_result", native_http_get_result},
+        {"http_set_bind_address", native_http_set_bind_address},
         {nullptr, nullptr}};
-};
 
+    static AsyncHttp asyncHttp; // Static instance of AsyncHttp
+};
 #endif
